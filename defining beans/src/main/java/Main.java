06a5ac86903ed618ce7238eed7d8f3a1a1295f7e
RehanChalana <rehanchalanaprsnl@gmail.com>
2024-01-28 19:26:29 +0530

    cleared files

--- defining beans/src/main/java/Main.java
+++ defining beans/src/main/java/Main.java
@@ -1,8 +1,8 @@
-import org.springframework.context.annotation.AnnotationConfigApplicationContextExtensionsKt;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         Parrot p = new Parrot();
     }
 }
