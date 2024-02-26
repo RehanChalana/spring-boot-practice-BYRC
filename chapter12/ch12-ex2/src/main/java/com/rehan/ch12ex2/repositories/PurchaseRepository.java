ecf52babe0b07a7e5fb7077408837a5562b8dc0b
RehanChalana <rehanchalanaprsnl@gmail.com>
2024-02-26 20:13:36 +0530

    completed ch12-ex2

---
+++ chapter12/ch12-ex2/src/main/java/com/rehan/ch12ex2/repositories/PurchaseRepository.java
@@ -0,0 +1,32 @@
+package com.rehan.ch12ex2.repositories;
+
+import com.rehan.ch12ex2.model.Purchase;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PurchaseRepository {
+    private final JdbcTemplate jdbc;
+    public PurchaseRepository(JdbcTemplate jdbc) {
+      this.jdbc=jdbc;
+    }
+
+    public void storePurchase(Purchase purchase) {
+        String sql = "INSERT INTO purchase (product,price) VALUES (?,?)";
+        jdbc.update(sql,purchase.getProduct(),purchase.getPrice());
+    }
+
+    public List<Purchase> findAllPurchase() {
+        String sql = "SELECT * FROM purchase";
+        RowMapper<Purchase> purchaseRowMapper = (r,i) -> {
+            Purchase rowObject = new Purchase();
+            rowObject.setId(r.getInt("id"));
+            rowObject.setProduct(r.getString("product"));
+            rowObject.setPrice(r.getBigDecimal("price"));
+            return rowObject;
+        };
+        return jdbc.query(sql,purchaseRowMapper);
+    }
+}
